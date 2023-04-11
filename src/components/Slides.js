46d1fa50e2b6285d5a5de56912c3168b0d432fc7
jairoalperez <jairoalberto98@hotmail.com>
2023-04-11 17:36:44 -0400

    Listo

--- src/components/Slides.js
+++ src/components/Slides.js
@@ -1,17 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function Slides({slides}) {
+
+function Slides({ slides }) {
+
+    const [slide, setSlide] = useState(0)
+    const [restart, setRestart] = useState(false)
+    const [prev, setPrev] = useState(false)
+    const [next, setNext] = useState(true)
+
+    const restartSlide = () => {
+        setSlide(0)
+        setRestart(false)
+        setPrev(false)
+        setNext(true)
+    }
+
+    const previousSlide = () => {
+        if (slide < 2) {
+            setPrev(false)
+            setRestart(false)
+        } else {
+            setPrev(true)
+            setRestart(true)
+            setNext(true)
+        }
+        setSlide(slide - 1)
+    }
+
+    const nextSlide = () => { 
+        if (slide < slides.length-2) {
+            setNext(true)
+            setRestart(true)
+            setPrev(true)
+        } else {
+            setNext(false)
+            setRestart(true)
+            setPrev(true)
+        }
+        setSlide(slide + 1)
+    }
 
     return (
         <div>
             <div id="navigation" className="text-center">
-                <button data-testid="button-restart" className="small outlined">Restart</button>
-                <button data-testid="button-prev" className="small">Prev</button>
-                <button data-testid="button-next" className="small">Next</button>
+                <button data-testid="button-restart" className="small outlined" onClick={restartSlide} disabled={!restart}>Restart</button>
+                <button data-testid="button-prev" className="small" onClick={previousSlide} disabled={!prev}>Prev</button>
+                <button data-testid="button-next" className="small" onClick={nextSlide} disabled={!next}>Next</button>
             </div>
             <div id="slide" className="card text-center">
-                <h1 data-testid="title">Slide Title Here</h1>
-                <p data-testid="text">Slide Text Here</p>
+                <h1 data-testid="title">{slides && slides[slide].title}</h1>
+                <p data-testid="text">{slides && slides[slide].text}</p>
             </div>
         </div>
     );
